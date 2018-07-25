255c492e832e530d7c54f2efab7c1224935c66e6
manuelubaldo <klauel.ubaldo@gmail.com>
2018-07-25 22:07:14 +0800

    Initial Commit.

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server,{
+    origins:'*:*'
+});
+
+const port = 3001;
+
+server.listen(port);
+
+// app.get('/',function(req, res) {
+//     res.send('Hello World');
+// })
+
+io.on('connection', function(socket){
+    console.log("connected");
+
+    socket.on('created',function(ticket) {
+        console.log(`created #${ticket}`);
+        io.sockets.emit('created',ticket);
+    })
+
+    socket.on('updated',function() {
+        console.log(`updated`);
+        io.sockets.emit('updated');
+    })
+})
+
